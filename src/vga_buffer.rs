a8bb3ed519ca11f4f5cec46a203390a39c7fbc0c
drakemain <demain1990@gmail.com>
2018-09-12 20:34:06 -0700

    handle newlines

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -64,11 +64,30 @@ impl Writer {
                 0x20...0x7e | b'\n' => self.write_byte(byte),
                 _ => self.write_byte(0xfe),
             }
-
         }
     }
 
-    fn new_line(&self) {}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank_char = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code
+        };
+
+        for i in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][i].write(blank_char);
+        }
+    }
 }
 
 pub fn print_something() {
@@ -81,7 +100,7 @@ pub fn print_something() {
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello! ");
+    writer.write_string("ello!\n ");
     write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
 
