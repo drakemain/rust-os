39c9629496637f8fec6181f5818d95d19ff7ccef
drakemain <demain1990@gmail.com>
2018-09-13 18:29:24 -0700

    run qemu with no gui

---
+++ run_nodisp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+qemu-system-x86_64 \
+    -drive format=raw,file=target/x86_64_rustos/debug/bootimage-rustos.bin \
+    -serial mon:stdio \
+    -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
+    -display none
