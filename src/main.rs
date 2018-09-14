314c27ab1b2e8d53e201c8e2244a63e0b2af771c
drakemain <demain1990@gmail.com>
2018-09-13 17:35:34 -0700

    basic unit testing setup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 #[macro_use]
 extern crate lazy_static;
 extern crate volatile;
 extern crate spin;
+#[cfg(test)]
+extern crate std;
+#[cfg(test)]
+extern crate array_init;
 
 use core::panic::PanicInfo;
 
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     for i in 0..100 {
@@ -21,6 +27,7 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
